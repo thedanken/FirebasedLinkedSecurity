78f46543974f49eddf2cdecc1de6227b9adde376
Daniel Kennelty <dkennelty@gmail.com>
2022-10-25 21:41:23 -0400

    Resolving Compilation Errors

--- app/src/main/java/com/example/firebasedlinkedsecurity/MainActivity.java
+++ app/src/main/java/com/example/firebasedlinkedsecurity/MainActivity.java
@@ -2,102 +2,20 @@ package com.example.firebasedlinkedsecurity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-import android.view.View;
-
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.firebasedlinkedsecurity.databinding.ActivityMainBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.view.*;
+import com.google.firebase.quickstart.auth.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-    private FirebaseAuth FBauth;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.v("","onCreate Started");
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        FBauth = FirebaseAuth.getInstance();
-        BeginSignInRequest signInRequest = BeginSignInRequest.builder()
-                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                        .setSupported(true)
-                        // Your server's client ID, not your Android client ID.
-                        .setServerClientId(getString(R.string.default_web_client_id))
-                        // Only show accounts previously used to sign in.
-                        .setFilterByAuthorizedAccounts(true)
-                        .build())
-                .build();
-
-        Log.v("","onCreate Ended");
-    }
-
-    public void onStart(){
-        super.onStart();
-        Log.v("","onStart Started");
-        FirebaseUser curUser = FBauth.getCurrentUser();
-        Log.v("","FB User grabbed");
-        if(curUser != null)
-        {
-            updateCurrentScreen("Connected");
-        }
-    }
-    public void updateCurrentScreen(String str) {
-        TextView Textfill = (TextView) this.findViewById(R.id.FirebaseConnected);
-        Textfill.setText(str);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+                .setGraph(R.navigation.nav_graph);
     }
 }
